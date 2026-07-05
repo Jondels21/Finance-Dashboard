9a38d20eb9f47f0d78a8b7513d89e77fc4e427c1
Janne Salovaara <jannesalovaara@gmail.com>
2026-07-05 18:07:11 +0300

    Added routes for pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,14 +5,41 @@ import RegisterPage from "./pages/RegisterPage";
 import DashboardPage from './pages/DashboardPage';
 
 import ProtectedRoute from './components/ProtectedRoute';
+import CategoriesPage from './pages/CategoriesPage';
+import TransactionsPage from './pages/TransactionsPage';
+import Layout from './components/Layout';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<ProtectedRoute><DashboardPage /></ProtectedRoute>} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/register" element={<RegisterPage />} />
+        <Route
+          path="/login"
+          element={<LoginPage />}
+        />
+
+        <Route
+          element={
+            <ProtectedRoute>
+              <Layout />
+            </ProtectedRoute>
+          }
+        >
+          <Route
+            path="/"
+            element={<DashboardPage />}
+          />
+
+          <Route
+            path="/categories"
+            element={<CategoriesPage />}
+          />
+
+          <Route
+            path="/transactions"
+            element={<TransactionsPage />}
+          />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
